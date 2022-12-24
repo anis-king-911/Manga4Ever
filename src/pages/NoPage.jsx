6353927a88992a56983ff97c6af9b4b8d654e96b
anis-king-911 <91683396+anis-king-911@users.noreply.github.com>
2022-12-25 00:37:42 +0100

    Delete src directory

--- src/pages/NoPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function NoPage() {
-  return <h1>404</h1>;
-};
-
-export default NoPage;
--- src/scripts/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-
-const app = document.querySelector('#app');
-const root = ReactDOM.createRoot(app);
-
-root.render(<App />);
