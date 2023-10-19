f48d469a008e50be66fa189727dcdb34d042bb64
anis-king-911 <91683396+anis-king-911@users.noreply.github.com>
2023-10-19 18:19:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const port = 3000;
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.use(express.static('./public/assets'));
 
